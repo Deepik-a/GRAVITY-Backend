3363260b11d2e94f10470f08fec223e0f2b593e1
Deepika <deepzsree22@gmail.com>
2026-04-25 09:06:33 +0530

    Some features

--- src/shared/constants/cookieData.ts
+++ src/shared/constants/cookieData.ts
@@ -4,7 +4,7 @@ export const cookieData = {
   httpONLY: true,
   SECURE: env.NODE_ENV === "production", 
   SAME_SITE: (env.NODE_ENV === "production" ? "none" : "lax") as "none" | "lax" | "strict", 
-  DOMAIN: env.NODE_ENV === "production" ? ".gravityconstruction.co.in" : "localhost",
+  DOMAIN: env.NODE_ENV === "production" ? "gravityconstruction.co.in" : "localhost",
   MAX_AGE_ACCESS_TOKEN: 15 * 60 * 1000, // 15 minutes
   MAX_AGE_REFRESH_TOKEN: 7 * 24 * 60 * 60 * 1000, // 7 days
 };
